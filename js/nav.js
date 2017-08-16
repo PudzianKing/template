e0628c8aa13ee6c88c428665888ad946efb2c5fa
PudzianKing <jmbatkowski@gmail.com>
2017-08-16 12:39:38 +0200

    menu refactor + content update

---
+++ js/nav.js
@@ -0,0 +1,54 @@
+var nav='<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">\n' +
+    '    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">\n' +
+    '    <span class="navbar-toggler-icon"></span>\n' +
+    '    </button>\n' +
+    '    <div class="container">\n' +
+    '    <a class="navbar-brand" href="index.html"><img src="img/nav/nav-logo.png" height="70px" alt="Graff"></a>\n' +
+    '    <div class="collapse navbar-collapse" id="navbarsExampleDefault">\n' +
+    '    <h1 class="menu-title">odzież ochronna dla służb leśnych</h1>\n' +
+    '<ul class="navbar-nav">\n' +
+    '    <li class="nav-item active ">\n' +
+    '    <a class="nav-link" href="index.html"><i class="fa fa-home" aria-hidden="true"></i></a>\n' +
+    '</li>\n' +
+    '<li class="nav-item dropdown">\n' +
+    '    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mężczyzna</a>\n' +
+    '    <div class="dropdown-menu" aria-labelledby="dropdown01">\n' +
+    '    <a class="dropdown-item" href="Kurtka_600.html">Kurtka</a>\n' +
+    '    <a class="dropdown-item" href="Spodnie_700.html">Spodnie</a>\n' +
+    '    <a class="dropdown-item" href="Koszula_829-KO.html">Koszula</a>\n' +
+    '    <a class="dropdown-item" href="Kamizelka_442-O.html">Kamizelka</a>\n' +
+    '    <a class="dropdown-item" href="Czapka_103.html">Czapka</a>\n' +
+    '    <a class="dropdown-item" href="Koszulka_959-OL.html">Koszulka</a>\n' +
+    '    </div>\n' +
+    '    </li>\n' +
+    '    <li class="nav-item dropdown">\n' +
+    '    <a class="nav-link dropdown-toggle" href="#" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kobieta</a>\n' +
+    '    <div class="dropdown-menu" aria-labelledby="dropdown02">\n' +
+    '    <a class="dropdown-item" href="Kurtka_600_k.html">Kurtka</a>\n' +
+    '    <a class="dropdown-item" href="Spodnie_700_k.html">Spodnie</a>\n' +
+    '    <a class="dropdown-item" href="Koszula_829-KO_k.html">Koszula</a>\n' +
+    '    <a class="dropdown-item" href="Kamizelka_442-O_k.html">Kamizelka</a>\n' +
+    '    <a class="dropdown-item" href="Czapka_103_k.html">Czapka</a>\n' +
+    '    <a class="dropdown-item" href="Koszulka_959-OL_k.html">Koszulka</a>\n' +
+    '    </div>\n' +
+    '    </li>\n' +
+    '    <li class="nav-item dropdown">\n' +
+    '    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Technologie</a>\n' +
+    '    <div class="dropdown-menu" aria-labelledby="dropdown03">\n' +
+    '    <a class="dropdown-item" href="Inscet_proof.html">InsectProof</a>\n' +
+    '    <a class="dropdown-item" href="dry_care.html">Dry Care</a>\n' +
+    '<a class="dropdown-item" href="uv_proof.html">UV Proof</a>\n' +
+    '<a class="dropdown-item" href="sub_zero.html">SUB ZERO</a>\n' +
+    '</div>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="gdzie_kupic.html">Gdzie kupić</a>\n' +
+    '</li>\n' +
+    '<li class="nav-item">\n' +
+    '    <a class="nav-link" href="obsluga.html">Obsługa klienta</a>\n' +
+    '</li>\n' +
+    '</ul>\n' +
+    '</div>\n' +
+    '</div>\n' +
+    '</nav>';
+    $( "#nav_hook" ).append( nav );
